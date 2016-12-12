514e7943f5e32b54c55c7c62c379fbe6942d99ae
boshek <sam.albers@gmail.com>
2016-12-12 09:17:13 -0800

    added time since, made all data on one line

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # output_sensor_T_H
+
+Instructions basically taken from here: http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-an-arduino/
--- output_sensor_T_H.ino
+++ output_sensor_T_H.ino
@@ -1,5 +1,7 @@
 #include <dht.h>
 
+#include <TimeLib.h>
+
 dht DHT;
 
 #define DHT11_PIN 7
@@ -11,11 +13,13 @@ void setup(){
 void loop()
 {
   int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Butts");
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
+  //Serial.println("Temperature = ");  
+  unsigned long currentTime = now(); // grab current time
+  Serial.print(DHT.temperature);
+  Serial.print(",");
+  Serial.print(DHT.humidity);
+  Serial.print(",");
+  Serial.println(currentTime);
+  delay(5000);
 }
 
